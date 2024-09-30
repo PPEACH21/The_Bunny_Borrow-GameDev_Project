d931075d07ca402ec3e1fee8812616523a772078
thitsanu25@gmail.com <>
2024-09-30 14:48:32 +0700

    add PauseUI, Music,Sound V1

---
+++ Assets/Script/IN_GAME/Pause/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+
+    }
+    public void Home()
+    {
+
+    }
+    public void Resume()
+    {
+
+    }
+    public void Restart()
+    {
+
+    }
+}
---
+++ Assets/Script/Sound/AudioManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public SoundFX[] musicSound, sfxSound;
+    public AudioSource musicSource, sfxSource;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("theme");
+    }
+    
+    
+    public void PlayMusic(string name)
+    {
+        SoundFX s = Array.Find(musicSound, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sond not found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+    public void PlaySFX(string name) 
+    {
+        SoundFX s = Array.Find(sfxSound, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sond not found");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+
+}
